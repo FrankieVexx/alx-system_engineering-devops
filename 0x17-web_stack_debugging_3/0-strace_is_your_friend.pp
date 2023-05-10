c1197f8b2ea255403156eee6d1c5492ba05ecb05
Francis Onyach <franblog7@gmail.com>
2023-05-10 22:08:26 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,13 @@
+# A puppet script to correct a cinfig file
 
+$file_path = '/var/www/html/wp-settings.php'
+
+#replace "phpp" with "php"
+
+exec { 'fix_line':
+
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+
+  path    => ['/bin','/usr/bin']
+
+}
