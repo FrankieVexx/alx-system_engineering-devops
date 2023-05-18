5a436a9c59971388d7cfc5b52145e949ca6ffb71
Francis Onyach <franblog7@gmail.com>
2023-05-18 07:33:09 -0700

    fix ginx traffit limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fixing nginx traffic limit to handle more than 2000 requests
+
+exec { 'ngix-fix':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,7 @@
+Webstack debugging 4. Increasing
+the limit for requests that can be made
+to an nginx server.
+The puppet lint file contains
+the commands used to edit ethe nginx configuration file
+in order to edit the default limit for the 
+accepted number of requests.
